7a3db5107889646593071433d62ea0f0e3ebe09a
Chino Nanami <tino_nanami@7mi.site>
2026-01-23 23:15:59 +0900

    debug

--- src/main/java/net/nicovrc/dev/HTTPServer.java
+++ src/main/java/net/nicovrc/dev/HTTPServer.java
@@ -134,8 +134,8 @@ public class HTTPServer extends Thread {
             file.mkdir();
         }
 
-        System.out.println("ffmpeg -i https://nicovrc.net" + uri + " -c:v copy -c:a copy -f hls -hls_playlist_type vod -hls_segment_filename \"/hls/"+s+"/%3d.ts\" /hls/"+s+"/main.m3u8");
-        ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", "ffmpeg -i https://nicovrc.net" + uri + " -c:v copy -c:a copy -f hls -hls_playlist_type vod -hls_segment_filename /hls/"+s+"/%3d.ts /hls/"+s+"/main.m3u8");
+        System.out.println("ffmpeg -i https://yobi.nicovrc.net" + uri + " -c:v copy -c:a copy -f hls -hls_playlist_type vod -hls_segment_filename \"/hls/"+s+"/%3d.ts\" /hls/"+s+"/main.m3u8");
+        ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", "ffmpeg -i https://yobi.nicovrc.net" + uri + " -c:v copy -c:a copy -f hls -hls_playlist_type vod -hls_segment_filename /hls/"+s+"/%3d.ts /hls/"+s+"/main.m3u8");
         Process process = pb.start();
         process.waitFor();
 
