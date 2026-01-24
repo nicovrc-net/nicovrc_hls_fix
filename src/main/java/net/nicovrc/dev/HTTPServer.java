8f70cb93eb128d37c976db9d994f00619bc947fe
Chino Nanami <tino_nanami@7mi.site>
2026-01-24 20:36:29 +0900

    update

--- src/main/java/net/nicovrc/dev/HTTPServer.java
+++ src/main/java/net/nicovrc/dev/HTTPServer.java
@@ -136,12 +136,16 @@ public class HTTPServer extends Thread {
         ProcessBuilder pb = new ProcessBuilder("/bin/ffmpeg", "-v","quiet","-i","https://yobi.nicovrc.net" + uri,"-c:v","copy","-c:a","copy","-f","hls","-hls_playlist_type","vod","-hls_segment_filename","/hls/"+s+"/%3d.ts","/hls/"+s+"/main.m3u8");
         Process process = pb.start();
 
-        new Thread(() -> { try (BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()))) { String l; while ((l = r.readLine()) != null) l = l; } catch (IOException ignored) {} }).start();
-        process.waitFor();
+        //new Thread(() -> { try (BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()))) { String l; while ((l = r.readLine()) != null) l = l; } catch (IOException ignored) {} }).start();
+        //process.waitFor();
 
         //System.out.println("debug");
 
-        System.out.println(new String(process.getErrorStream().readAllBytes(), StandardCharsets.UTF_8));
+        //System.out.println(new String(process.getErrorStream().readAllBytes(), StandardCharsets.UTF_8));
+
+        while (!new File("/hls/"+s+"/main.m3u8").exists()){
+            Thread.sleep(10L);
+        }
 
         String hlsText = null;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("/hls/"+s+"/main.m3u8"), StandardCharsets.UTF_8))){
