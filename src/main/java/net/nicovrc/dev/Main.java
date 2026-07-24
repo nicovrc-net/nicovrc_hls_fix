daac81a2a2539a9254a09fbe5f00400747d8b279
Chino Nanami <tino_nanami@7mi.site>
2026-07-24 13:18:47 +0900

    dummy

---
+++ src/main/java/net/nicovrc/dev/Main.java
@@ -0,0 +1,9 @@
+package net.nicovrc.dev;
+
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
