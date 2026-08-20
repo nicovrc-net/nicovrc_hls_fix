9516c48e94ad8707fc019b676ea5af6d1c839701
Chino Nanami <tino_nanami@7mi.site>
2026-08-20 20:08:55 +0900

    間違って消してた

--- src/main/java/net/nicovrc/dev/Main.java
+++ src/main/java/net/nicovrc/dev/Main.java
@@ -4,6 +4,14 @@ package net.nicovrc.dev;
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        HTTPServer httpServer = new HTTPServer();
+        try {
+            httpServer.start();
+            httpServer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
